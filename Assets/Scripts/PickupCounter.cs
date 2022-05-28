7a8bf8dd270092fc1dacd003d1b16d8ba1037b82
melanie <melanie@cooperbold.com>
2022-05-28 00:54:05 -0700

    Add scripts for picking up and counting items... almost works

--- Assets/Scripts/PickupCounter.cs
+++ Assets/Scripts/PickupCounter.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class PickupCounter : MonoBehaviour
 {
     public int batteryCount;
-    public int GlowStickCount;
+    public int glowStickCount;
 
     void OnTriggerEnter(Collider collider) {
-        Debug.Log("Collided with something");
+        print(collider.name);
 
         // Check to see if the item is a pickup
-        if (collider.gameObject.tag == "Pickup")
+        if (collider.tag == "Pickup")
         {
             Debug.Log("Collided with a pickup item");
+
+            // Increment counter based on item type
+            if(collider.GetComponent<PickableItem>().isBattery) {
+                Debug.Log("Battery!");
+                batteryCount += collider.GetComponent<PickableItem>().numItems;
+            }
+            if(collider.GetComponent<PickableItem>().isGlowStick) {
+                Debug.Log("Glowstick!");
+                glowStickCount += collider.GetComponent<PickableItem>().numItems;
+            }
         }
     }
 }
