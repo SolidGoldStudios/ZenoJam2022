13e49b59b9e10acde356b4d49ad27d9b315ef6d1
Dustin Cooper <dcooper@deepthought.org>
2022-05-28 18:13:55 -0700

    doorway opened by switch

--- Assets/Scripts/LightUp.cs
+++ Assets/Scripts/LightUp.cs
@@ -7,6 +7,10 @@ public class LightUp : MonoBehaviour
     private Material mat;
     public Color color;
     public float intensity = 0;
+    public bool isSwitch;
+    public GameObject switchTarget;
+
+    private bool switchFlipped = false;
 
     void Start()
     {
@@ -18,11 +22,12 @@ public class LightUp : MonoBehaviour
     {
         if (intensity > 0) {
             intensity -= Time.deltaTime / 10;
-        } else {
-            intensity = 0;
-        }
 
-        mat.SetColor("_EmissionColor", color * intensity);
+            if (intensity < 0.5f && isSwitch && switchFlipped) intensity = 0.5f;
+            if (intensity < 0) intensity = 0;
+
+            mat.SetColor("_EmissionColor", color * intensity);
+        }
     }
 
     public void Glow()
@@ -30,6 +35,13 @@ public class LightUp : MonoBehaviour
         if (intensity < 1.5f) {
             // intensity += 0.001f;
             intensity += Time.deltaTime / 2;
+        }
+
+        if (intensity > 0.5f && isSwitch && !switchFlipped) {
+            MoveableObject mov = switchTarget.GetComponent<MoveableObject>();
+            if (mov) mov.Move();
+
+            switchFlipped = true;
         }
     }
 }
