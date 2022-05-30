33506abc2cf8a46cd186c97c9f47fced52742ccc
Adam E <adamgemerson@gmail.om>
2022-05-30 01:21:44 -0500

    Switch update to crystal

--- Assets/DiscoRotate.cs
+++ Assets/DiscoRotate.cs
@@ -6,15 +6,32 @@ public class DiscoRotate : MonoBehaviour
 {
 
     public float speed = 10f;
+    public float levitate = 10f;
+
+    private Vector3 startPosition;
+
+    private bool atStart = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0f, speed * Time.deltaTime, 0f);
+        
+        if (startPosition.y - levitate > transform.position.y) 
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + (5 * Time.deltaTime), transform.position.z);
+        }
+
+        if (startPosition.y + levitate < transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - (5 * Time.deltaTime), transform.position.z);
+        }
+
+        
     }
 }
