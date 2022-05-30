f4ccf94c54b251467f24286c1e65dfcf36dc3dad
Dustin Cooper <dcooper@deepthought.org>
2022-05-29 22:37:01 -0700

    hopefully fixed initial mouse look fuckyness

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -23,10 +23,10 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSense * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSense * Time.deltaTime;
 
-        if (Mathf.Abs(mouseX) > 500 || Mathf.Abs(mouseY) > 500) return;
+        if (Mathf.Abs(mouseX) > 100 || Mathf.Abs(mouseY) > 100) return;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
