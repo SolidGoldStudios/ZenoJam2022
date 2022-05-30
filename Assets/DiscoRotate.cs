5983fc334cfeb0dfa88e64d9840656f3f46c563f
Adam E <adamgemerson@gmail.om>
2022-05-30 01:35:58 -0500

    Levitating switch

--- Assets/DiscoRotate.cs
+++ Assets/DiscoRotate.cs
@@ -7,7 +7,8 @@ public class DiscoRotate : MonoBehaviour
 
     public float speed = 10f;
     public float levitate = 10f;
-
+    public float amplitude = 10f;
+    
     private Vector3 startPosition;
 
     private bool atStart = true;
@@ -21,17 +22,9 @@ public class DiscoRotate : MonoBehaviour
     void Update()
     {
         transform.Rotate(0f, speed * Time.deltaTime, 0f);
-        
-        if (startPosition.y - levitate > transform.position.y) 
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y + (5 * Time.deltaTime), transform.position.z);
-        }
 
-        if (startPosition.y + levitate < transform.position.y)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y - (5 * Time.deltaTime), transform.position.z);
-        }
+        transform.position = new Vector3(transform.position.x, startPosition.y + amplitude * Mathf.Sin(levitate*Time.time), transform.position.z);
 
-        
+
     }
 }
